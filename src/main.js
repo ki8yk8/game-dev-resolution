2a003a2f062ab5c30db4b5c6c2bdc924868ffe26
ki8yk8 <ki8yk8@gmail.com>
2026-03-13 13:18:55 +0545

    adds player movement

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import kaplay from "kaplay";
 
-const k = kaplay();
+const k = kaplay({
+	background: "#eeeeff",
+});
 
 k.loadRoot("./");
-k.loadSprite("bean", "sprites/bean.png");
+k.loadSprite("bean", "/sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+const PLAYER_SPEED = 200; // in pixels/second
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+const player = k.add([k.pos(120, 80), k.sprite("bean")]);
+player.onUpdate(() => {
+	let currrentPosition = player.pos;
+
+	if (k.isKeyPressed("left")) {
+		currrentPosition = currrentPosition.add(-PLAYER_SPEED*k.dt(),0)
+	}
+	if (k.isKeyPressed("right")) {
+		currrentPosition = currrentPosition.add(PLAYER_SPEED*k.dt(),0)
+	}
+	if (k.isKeyPressed("up")) {
+		currrentPosition = currrentPosition.add(0, -PLAYER_SPEED*k.dt())
+	}
+	if (k.isKeyPressed("down")) {
+		currrentPosition = currrentPosition.add(0, PLAYER_SPEED*k.dt())
+	}
+
+	player.pos = currrentPosition;
+})
