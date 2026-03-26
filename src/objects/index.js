58c77e3032119e58d7653d14a7ad8e56c471b315
ki8yk8 <ki8yk8@gmail.com>
2026-03-26 23:11:06 +0545

    modulaizes the code for easier debugging

--- src/objects/index.js
+++ src/objects/index.js
@@ -1,3 +1,4 @@
 export * from "./player";
 export * from "./wall";
 export * from "./objects";
+export * from "./map";
--- src/scenes/gameplay.js
+++ src/scenes/gameplay.js
@@ -1,4 +1,4 @@
-import { Player, Wall, Objects } from "../objects";
+import { Player, Map, Objects } from "../objects";
 import { WELCOME_MESSAGE } from "../data/scripts";
 import { Message } from "../ui";
 
@@ -7,6 +7,9 @@ async function GamePlay({ k, c }) {
 		k,
 		c,
 	});
+	Map({ k, c });
+
+	// collection holds all the items stored
 	const collection = [];
 
 	// disables player movement while the welcome message is being displayed
@@ -26,55 +29,8 @@ async function GamePlay({ k, c }) {
 	// enable player movement after all welcome message finishes
 	enablePlayerMovement();
 
-	Wall({ k, c, width: 1200, pos: k.vec2(100, 100) });
-	const retractableWall = Wall({
-		k,
-		c,
-		width: 200,
-		pos: k.vec2(100 + 1200, 100),
-		isRetractable: true,
-	}); // door
-	Wall({
-		k,
-		c,
-		width: 200,
-		pos: k.vec2(100 + 1200, 100 - 200),
-		isVertical: true,
-	});
-	Wall({
-		k,
-		c,
-		width: 200,
-		pos: k.vec2(100 + 1200 + 232, 100 - 200),
-		isVertical: true,
-	});
-	Wall({ k, c, width: 200, pos: k.vec2(100 + 1200, -100) });
-
-	Wall({ k, c, width: 618, pos: k.vec2(100 + 1200 + 200, 100) });
-	Wall({ k, c, width: 600, pos: k.vec2(100, 100), isVertical: true });
-	Wall({ k, c, width: 700, pos: k.vec2(100, 700 - 32) });
-	Wall({ k, c, width: 300, pos: k.vec2(800, 400), isVertical: true });
-	Wall({ k, c, width: 150, pos: k.vec2(800, 400) });
-	Wall({ k, c, width: 600, pos: k.vec2(950, 400), isVertical: true });
-	Wall({ k, c, width: 400, pos: k.vec2(550, 1000) });
-	Wall({ k, c, width: 200, pos: k.vec2(550, 1000), isVertical: true });
-	Wall({ k, c, width: 700, pos: k.vec2(518, 1200) });
-	Wall({ k, c, width: 500, pos: k.vec2(1218, 700), isVertical: true });
-	Wall({ k, c, width: 500, pos: k.vec2(1218, 700) });
-	Wall({ k, c, width: 500, pos: k.vec2(1718, 700), isVertical: true });
-	Wall({ k, c, width: 600, pos: k.vec2(1686, 1200) });
-	Wall({ k, c, width: 200, pos: k.vec2(2300, 1032), isVertical: true });
-	Wall({ k, c, width: 150, pos: k.vec2(2300 - 150, 1032) });
-	Wall({
-		k,
-		c,
-		width: 966,
-		pos: k.vec2(2300 - 150, 1032 - 966 + 32),
-		isVertical: true,
-	});
-
 	// render the scene that plays the game
-	const win = Objects({
+	Objects({
 		k,
 		c,
 		deps: { disablePlayerMovement, enablePlayerMovement, collection },
