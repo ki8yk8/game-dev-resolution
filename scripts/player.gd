663dfd51db0179c8c5b8824968b01318917a5a3f
ki8yk8 <ki8yk8@gmail.com>
2026-03-31 19:25:40 +0545

    adds different sprite animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
@@ -11,12 +12,29 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if !is_on_floor():
+		animated_sprite.play("jump")
+		
+	# directon = -ve, 0, +ve
+	var direction := Input.get_axis("move_left", "move_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good pra    ctice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	#change the facing of the sprite
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+		
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
