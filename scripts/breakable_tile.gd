2681fb82762088d7a3eb188451daed2d9dd54919
ki8yk8 <ki8yk8@gmail.com>
2026-03-31 18:41:26 +0545

    breakable tile now works by utilizing node2d instead of just node

--- scripts/breakable_tile.gd
+++ scripts/breakable_tile.gd
@@ -1,11 +1,13 @@
-extends Node
+extends Node2D
 
+@onready var timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("landed here")
+	timer.start()
 
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	timer.stop()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	queue_free()
