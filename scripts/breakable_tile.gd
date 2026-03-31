8023595046a228e3900d118f6c11e412a7683ad4
ki8yk8 <ki8yk8@gmail.com>
2026-03-31 18:27:57 +0545

    unable to complete the breakable tile and hence reverts to use of classic tile

--- scripts/breakable_tile.gd
+++ scripts/breakable_tile.gd
@@ -1,10 +1,11 @@
-extends Area2D
+extends Node
 
-@onready var timer = $Timer
 
-func _on_body_entered(body: Node2D) -> void:
-	timer.start()
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _on_timer_timeout() -> void:
-	# break the tile
-	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
