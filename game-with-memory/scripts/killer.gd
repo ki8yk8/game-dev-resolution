53f50953fa8bf072dc39e7783396c45b1201ce68
ki8yk8 <ki8yk8@gmail.com>
2026-04-19 19:45:17 +0545

    killer now works with killing features

--- game-with-memory/scripts/killer.gd
+++ game-with-memory/scripts/killer.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var DeathTimer: Timer = $DeathTimer
 
-const SPEED = 100
+const SPEED = 75
 var started = false
 
 func _ready():
@@ -12,12 +12,12 @@ func _process(delta: float) -> void:
 	if started:
 		position.x += SPEED*delta
 
-func _on_killer_body_entered(body: Node2D) -> void:
+
+
+func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.change_animation("death")
 		DeathTimer.start()
-
-
 
 func _on_death_timer_timeout() -> void:
 	DeathTimer.stop()
