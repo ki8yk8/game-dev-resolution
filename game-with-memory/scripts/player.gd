2f63af69eeba66f88559bd3c649fb089295b74cf
ki8yk8 <ki8yk8@gmail.com>
2026-04-19 16:44:14 +0545

    adds constant x axis velocity for the player

--- game-with-memory/scripts/player.gd
+++ game-with-memory/scripts/player.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 @onready var TopLeftMarker = %CameraMarkers/TopLeft
 @onready var BottomRightMarker = %CameraMarkers/BottomRight
 @onready var Camera = $Camera2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -225.0
 
 func _ready() -> void:
 	# utilizing the camera markers fix the camera boundaries
@@ -18,20 +19,15 @@ func _ready() -> void:
 	Camera.limit_right = bottomRightPos[0]
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# creating the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# handling the jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# constant forward movement of the player
+	velocity.x = SPEED
 
 	move_and_slide()
