75f668c74072fb414e8878de437f3225e0083fcc
ki8yk8 <ki8yk8@gmail.com>
2026-04-22 22:59:46 +0545

    add score feature

--- game-with-memory/scripts/game_play.gd
+++ game-with-memory/scripts/game_play.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @onready var timer = $StartTimer/Timer
 @onready var start_label = $StartTimer/Label
+@onready var score_label = $GameHUD/Control/MarginContainer/HBoxContainer/MarginContainer/ScoreHbox/Label
 
 const START_TIMER_STATES = ["3", "2", "1", "START..."]
 var start_timer_state = 0
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +16,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if GlobalScript.gameplay_started:
+		score += 1
+		score_label.text = "Score: "+str(score)
 
 func _on_timer_timeout() -> void:
 	if start_timer_state == len(START_TIMER_STATES)-1:
