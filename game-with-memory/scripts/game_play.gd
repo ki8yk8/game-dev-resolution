ae46c7458c20dd9babda371a83d3f93519e10cd4
ki8yk8 <ki8yk8@gmail.com>
2026-04-22 22:51:47 +0545

    adds timer on the game play with user notification

---
+++ game-with-memory/scripts/game_play.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var timer = $StartTimer/Timer
+@onready var start_label = $StartTimer/Label
+
+const START_TIMER_STATES = ["3", "2", "1", "START..."]
+var start_timer_state = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GlobalScript.gameplay_started = false
+	timer.start()
+	start_label.text = START_TIMER_STATES[start_timer_state]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_timer_timeout() -> void:
+	if start_timer_state == len(START_TIMER_STATES)-1:
+		timer.stop()
+		start_label.queue_free()
+		GlobalScript.gameplay_started = true
+	else:
+		start_timer_state += 1
+		start_label.text = START_TIMER_STATES[start_timer_state]
--- game-with-memory/scripts/killer.gd
+++ game-with-memory/scripts/killer.gd
@@ -3,23 +3,18 @@ extends Area2D
 @onready var DeathTimer: Timer = $DeathTimer
 
 const SPEED = 75
-var started = false
-
-func _ready():
-	DeathTimer.start()
 
 func _process(delta: float) -> void:
-	if started:
+	if GlobalScript.gameplay_started:
 		position.x += SPEED*delta
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.change_animation("death")
 		DeathTimer.start()
+		GlobalScript.gameplay_started = false
 
 func _on_death_timer_timeout() -> void:
 	DeathTimer.stop()
-	if started:
-		get_tree().reload_current_scene()
-	else:
-		started = true
+	# TODO: the next scene should be displayed here
+	get_tree().reload_current_scene()
