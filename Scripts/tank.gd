f9dcf9e0011af91bd7e93e77e8966a91b0892f1a
ki8yk8 <ki8yk8@gmail.com>
2026-05-10 19:16:24 +0545

    makes the ride more smooth

--- Scripts/tank.gd
+++ Scripts/tank.gd
@@ -3,23 +3,23 @@ extends CharacterBody2D
 const SPEED = 150.0
 const ROTATION_SPEED = 2.0
 const ACCELERATION = 100.0
-const FRICTION = 8.0
+const FRICTION = 0.6    # between 0 and 1 where, 0 is complete stop and 1 is slippery
+const MAX_VELOCITY = Vector2(100.0, 100.0)
 
 func _physics_process(delta: float) -> void:
-	var input_dir := Input.get_axis("up", "down")
+	var input_dir := Input.get_axis("down", "up")
 	var rotate_dir := Input.get_axis("left", "right")
 	
 	rotation += rotate_dir * ROTATION_SPEED * delta
-	
-	# v = u + at here, v, u = is delta
-	# y axis = cosine component and x axis sine component if theta is angle between the straight and the pointing directionn
-	var rotation_radian = deg_to_rad(rotation)
-	var a_y = ACCELERATION * cos(rotation_radian)
-	var a_x = ACCELERATION * sin(rotation_radian)
+	var direction = Vector2.UP.rotated(rotation)
 	
 	if input_dir != 0.0:
-		velocity += Vector2(a_x, a_y) * delta * input_dir
+		velocity += direction * ACCELERATION * delta * input_dir
 	else:
-		pass
-		
+		velocity *= FRICTION
+		# prevent micro sliding
+		if velocity.length() < 5.0:
+			velocity = Vector2.ZERO
+	velocity = velocity.clamp(-MAX_VELOCITY, MAX_VELOCITY)
+	
 	move_and_slide()
