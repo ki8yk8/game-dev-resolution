0a1299f1cdb38abf193f6c254647ee9d4de18094
ki8yk8 <ki8yk8@gmail.com>
2026-05-11 22:25:49 +0545

    adds physics interaction when slippery oil is touched by the tank

--- Scripts/tank.gd
+++ Scripts/tank.gd
@@ -1,18 +1,40 @@
 extends CharacterBody2D
 
+@onready var oilspill_tilemap = $"../ObjectsMap/28x28 Tiles"
+@onready var timer = $Timer
+
 const ROTATION_SPEED = 2.0
 const ACCELERATION = 300.0
 const FRICTION = 0.85    # between 0 and 1 where, 0 is complete stop and 1 is slippery
 const LATERAL_FRICTION = 0.05
 const MAX_VELOCITY = 100.0
+const SLIPPERY_TIME = 4
 
 const BULLET_SCENE = preload("res://Scenes/bullet.tscn")
 
+var slippery = false
+
 func _physics_process(delta: float) -> void:
+	var cell = oilspill_tilemap.local_to_map(oilspill_tilemap.to_local(global_position))
+	var tile_data = oilspill_tilemap.get_cell_tile_data(cell)
+	
+	if tile_data:
+		slippery = tile_data.get_custom_data("is_oil")
+		timer.start(SLIPPERY_TIME)
+		
+	if slippery:
+		print("Slippery")
+	
+	var acceleration = ACCELERATION if not slippery else ACCELERATION * 2
+	var friction = FRICTION if not slippery else FRICTION * 0.5
+	var lateral_friction = LATERAL_FRICTION if not slippery else LATERAL_FRICTION * 0.5
+	var max_velocity = MAX_VELOCITY if not slippery else MAX_VELOCITY * 1.5
+	var rotation_speed = ROTATION_SPEED if not slippery else ROTATION_SPEED * 2
+	
 	var input_dir := Input.get_axis("down", "up")
 	var rotate_dir := Input.get_axis("left", "right")
 	
-	rotation += rotate_dir * ROTATION_SPEED * delta
+	rotation += rotate_dir * rotation_speed * delta
 	# manage the bullets
 	if Input.is_action_just_pressed("shoot"):
 		var bullet = BULLET_SCENE.instantiate()
@@ -27,17 +49,21 @@ func _physics_process(delta: float) -> void:
 	
 	var forward_vel = forward * velocity.dot(forward)
 	var lateral_vel = right * velocity.dot(right)
-	lateral_vel *= LATERAL_FRICTION
+	lateral_vel *= lateral_friction
 	
 	if input_dir != 0.0:
-		forward_vel += forward * ACCELERATION * delta * input_dir
+		forward_vel += forward * acceleration * delta * input_dir
 	else:
-		forward_vel *= FRICTION
+		forward_vel *= friction
 		# prevent micro sliding
 		if forward_vel.length() < 5.0:
 			forward_vel = Vector2.ZERO
 	
 	velocity = forward_vel + lateral_vel
-	velocity = velocity.limit_length(MAX_VELOCITY)
+	velocity = velocity.limit_length(max_velocity)
 	
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	timer.stop()
+	slippery = false
