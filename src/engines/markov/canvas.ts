2c51d7ad12c8a6a103c288a0573174fdbee5fdab
ki8yk8 <ki8yk8@gmail.com>
2026-05-24 23:26:56 +0545

    restructure canvas for better editing and debugging

--- src/engines/markov/canvas.ts
+++ src/engines/markov/canvas.ts
@@ -12,31 +12,33 @@ export default function MarkovCanvas(props: MarkovCanvasProps): HTMLElement {
 	// title for the panel
 	const title = document.createElement("p");
 	title.className = "dashboard__canvas__title";
-	title.textContent = "Markov Panel";
+	canvas.appendChild(title);
 
 	// district status
 	const districtStatus = document.createElement("p");
 	districtStatus.className = "markov__status";
-	districtStatus.textContent = `${props.district.name} - Current State: ${props.district.state}`;
+	canvas.appendChild(districtStatus);
 
 	// long run forecast
 	const longForecastDiv = document.createElement("div");
 	longForecastDiv.className = "markov__steady";
+	canvas.appendChild(longForecastDiv);
 
 	const longForecastTitle = document.createElement("p");
-	longForecastTitle.textContent = "Long-run Forecast";
 	longForecastTitle.className = "markov__steady__title";
 	longForecastDiv.appendChild(longForecastTitle);
 
 	const longForecastBody = document.createElement("p");
 	longForecastBody.className = "markov__steady__content";
-	const forecast = props.district.longForecast();
-	longForecastBody.textContent = `Stable: ${Math.floor(forecast.Stable * 100)}%, Tense: ${Math.floor(forecast.Tense * 100)}%, Riot: ${Math.floor(forecast.Riot * 100)}%, Recover: ${Math.floor(forecast.Recovery * 100)}%`;
 	longForecastDiv.appendChild(longForecastBody);
 
-	canvas.appendChild(title);
-	canvas.appendChild(districtStatus);
-	canvas.appendChild(longForecastDiv);
+	// content starts here
+	title.textContent = "Markov Panel";
+	districtStatus.textContent = `${props.district.name} - Current State: ${props.district.state}`;
+	longForecastTitle.textContent = "Long-run Forecast";
+
+	const forecast = props.district.longForecast();
+	longForecastBody.textContent = `Stable: ${Math.floor(forecast.Stable * 100)}%, Tense: ${Math.floor(forecast.Tense * 100)}%, Riot: ${Math.floor(forecast.Riot * 100)}%, Recover: ${Math.floor(forecast.Recovery * 100)}%`;
 
 	return canvas;
 }
