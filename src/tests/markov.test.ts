f8f9fe5c7a6dc5884e9300b0823477862bdbc0fa
ki8yk8 <ki8yk8@gmail.com>
2026-05-24 21:48:19 +0545

    adds test for the markov engine

---
+++ src/tests/markov.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+import { DistrictStats } from "../components/districts";
+import { MarkovEngine } from "../engines/markov/engine";
+
+describe("Markov engine should handle all the state transitions", () => {
+	const dummystats: DistrictStats = {
+		infectionRate: 0.2,
+		crimeIndex: 0.4,
+		infraHealth: 0.1,
+		socialTension: 0.1,
+	};
+
+	const engine = new MarkovEngine(dummystats);
+
+	test("Next state should be able to be determined from transition matrix", () => {
+		const nextState = engine.nextState("Recovery");
+
+		console.log(nextState);
+		expect(nextState).toBeOneOf(["Recovery", "Riot", "Stable", "Tense"]);
+	});
+
+	test("Steady state should give probailities of ending up in a state", () => {
+		const steadyState = engine.steadyState();
+
+		console.log(steadyState);
+		expect(Object.keys(steadyState)).toStrictEqual([
+			"Recovery",
+			"Riot",
+			"Stable",
+			"Tense",
+		]);
+	});
+});
