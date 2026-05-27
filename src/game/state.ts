68f01e61485cee1b6a0c0583a5c85c6bbe9ec3bb
ki8yk8 <ki8yk8@gmail.com>
2026-05-27 22:01:07 +0545

    adds class for storing the game state

---
+++ src/game/state.ts
@@ -0,0 +1,30 @@
+import type { Purchase } from "./type";
+
+const INITIAL_TOKEN = 100;
+
+class GameState {
+	tokens: number;
+	private purchase_history: Purchase[];
+
+	constructor() {
+		this.tokens = INITIAL_TOKEN;
+		this.purchase_history = [];
+	}
+
+	public purchase = (particular: string, tokens: number) => {
+		if (this.tokens < tokens) {
+			throw new Error("Couldn't complete the purchase. Infsufficient tokens.");
+		}
+
+		this.purchase_history.push({
+			particular,
+			tokens,
+		});
+
+		this.tokens -= tokens;
+	};
+
+	public getPurchaseHistory = () => {
+		return this.purchase_history;
+	};
+}
---
+++ src/game/type.ts
@@ -0,0 +1,4 @@
+export interface Purchase {
+	tokens: number;
+	particular: string;
+}
