ba4b2f065ac8d8c4c03c7d6fcb0cf9eb0afda34f
ki8yk8 <ki8yk8@gmail.com>
2026-06-05 00:57:34 +0545

    adds triggering and shooting logic

--- scripts/push_trap_shooter.gd
+++ scripts/push_trap_shooter.gd
@@ -1,2 +1,10 @@
 extends Node2D
 class_name PushTrapShooter
+
+@export var shooterSword: PackedScene = preload("res://scenes/shooter_sword.tscn")
+
+func shoot():
+	var shooter = shooterSword.instantiate()
+	get_tree().current_scene.add_child(shooter)
+	shooter.global_position = global_position
+	shooter.rotation = rotation
--- scripts/push_trap_trigger.gd
+++ scripts/push_trap_trigger.gd
@@ -2,3 +2,18 @@ extends Area2D
 class_name PushTrapTrigger
 
 var shooters = []
+var cooldown: bool = false
+
+func _on_body_entered(body: Node2D) -> void:
+	# if this is cooldown then, donot do anythng
+	if cooldown:
+		return
+	# cooldown activates; after time is over turnoff the cooldown
+	cooldown = true
+	
+	# shoot from each shooter
+	for shooter in shooters:
+		shooter.shoot()
+
+func _on_timer_timeout() -> void:
+	cooldown = false
