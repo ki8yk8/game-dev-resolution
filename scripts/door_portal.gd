3c528987ada9dd42e38894a26882b1e7a45a38be
ki8yk8 <ki8yk8@gmail.com>
2026-06-04 22:30:14 +0545

    doorportal recognizes the two doors which is it's children

--- scripts/door_portal.gd
+++ scripts/door_portal.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+var doors = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var childrens = get_children()
-	
+	doors = childrens.filter(func (child):
+		return child is SmallDoor or child is BigDoor
+	)
+	# there should be always two doors. One to enter another to leave while viceversa also shall work
+	assert(doors.size() == 2)
