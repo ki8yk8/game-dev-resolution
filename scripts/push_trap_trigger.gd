58665709acca971b396eb200aa6cd49515983848
ki8yk8 <ki8yk8@gmail.com>
2026-06-05 01:00:17 +0545

    connects timer to push trap shooter

--- scripts/push_trap_trigger.gd
+++ scripts/push_trap_trigger.gd
@@ -4,12 +4,15 @@ class_name PushTrapTrigger
 var shooters = []
 var cooldown: bool = false
 
+@onready var timer = $Timer
+
 func _on_body_entered(body: Node2D) -> void:
 	# if this is cooldown then, donot do anythng
 	if cooldown:
 		return
 	# cooldown activates; after time is over turnoff the cooldown
 	cooldown = true
+	timer.start()
 	
 	# shoot from each shooter
 	for shooter in shooters:
