b71482b5af417e6fe392e1cb15220dcdb990ea5a
ki8yk8 <ki8yk8@gmail.com>
2026-06-05 00:48:37 +0545

    adds puhstrap to the gameplay

--- scripts/push_trap.gd
+++ scripts/push_trap.gd
@@ -4,13 +4,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var children = get_children()
-	var trapTriggers: Array[PushTrapTrigger] = children.filter(func (item):
+	var trapTriggers = children.filter(func (item):
 		return item is PushTrapTrigger
 	)
-	var trapShooters: Array[PushTrapShooter] = children.filter(func (item):
+	var trapShooters = children.filter(func (item):
 		return item is PushTrapShooter
 	)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+	# trigger should know the shooters
+	for trapTrigger in trapTriggers:
+		trapTrigger.shooters = trapShooters
