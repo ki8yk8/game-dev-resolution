ebf14998a1de36bd09f37286ffc7684e1701aa38
ki8yk8 <ki8yk8@gmail.com>
2026-06-05 00:21:51 +0545

    adds shooter with cone raycast that hits the sword

---
+++ scripts/shooter.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const ROTATION_SPEED = 30
+
+@onready var raycast:Area2D = $Raycast
+@onready var shootingOrigin: Marker2D = $Marker2D
+@export var shooter_scene: PackedScene = preload("res://scenes/shooter_sword.tscn")
+
+func _process(delta: float) -> void:
+	# rotat the raycast continuously
+	raycast.rotation_degrees += ROTATION_SPEED*delta
+	if raycast.rotation_degrees > 360:
+		raycast.rotation_degrees = raycast.rotation_degrees-360
+
+func _on_raycast_body_entered(body: Node2D) -> void:
+	var bullet:Area2D = shooter_scene.instantiate()
+	get_tree().current_scene.add_child(bullet)
+	bullet.global_position = shootingOrigin.global_position
+	bullet.rotation_degrees = raycast.rotation_degrees+90
---
+++ scripts/shooter_sword.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+const VELOCITY = 100
+@onready var timer:Timer = $Timer
+
+func _ready() -> void:
+	timer.start()
+
+func _process(delta: float) -> void:
+	# controls the movement
+	position -= Vector2(0, VELOCITY).rotated(rotation)*delta
+	
+func _on_timer_timeout() -> void:
+	# remove the sword on timeout
+	timer.stop()
+	queue_free()
