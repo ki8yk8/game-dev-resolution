4d26f1635265e6bbaf63cdc81a49cd03d829fd7f
ki8yk8 <ki8yk8@gmail.com>
2026-06-06 14:16:31 +0545

    adds spider enraged animation

--- scripts/spider.gd
+++ scripts/spider.gd
@@ -4,12 +4,17 @@ extends Area2D
 const SPEED = 50
 
 @onready var raycast: RayCast2D = $RayCast2D
+@onready var cooldownTimer: Timer = $CooldownTimer
+@onready var sprite2d:Sprite2D =  $Sprite2D
+@onready var animationPlayer:AnimationPlayer = $AnimationPlayer
+
+# when enraged the things becomes enlarged and starts to follow the person to kill
+var enraged: bool = false
 
 func _process(delta: float) -> void:
 	position += Vector2(SPEED, 0).rotated(rotation)*delta
 	
 	if raycast.is_colliding():
-		print("Here")
 		rotate_randomly()
 
 func rotate_randomly():
@@ -17,5 +22,16 @@ func rotate_randomly():
 	var random_rotations = [90, 180, 270]
 	rotation += deg_to_rad(random_rotations.pick_random())
 	
+# for the trigger collision
 func _on_body_entered(body: Node2D) -> void:
-	pass
+	# the thing gets enraged
+	cooldownTimer.start()
+	animationPlayer.play("scale")
+
+# for the kill collision
+# TODO: add the player die thing here
+func _on_kill_area_body_entered(body: Node2D) -> void:
+	print("You died")
+
+func _on_cooldown_timer_timeout() -> void:
+	animationPlayer.stop()
