ef6970bf5b1145fcfb6bbda6ae5a38a7d39ba797
ki8yk8 <ki8yk8@gmail.com>
2026-06-06 14:41:28 +0545

    smooth tranistion of spider from transition to non-transition phase

--- scripts/spider.gd
+++ scripts/spider.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 # only one should be on at a time to have the linear motion
 const SPEED = 50
+const ENRAGED_SPEED = 30
+const ANGLES = [0, 90, 180, 270]
 
 @onready var raycast: RayCast2D = $RayCast2D
 @onready var cooldownTimer: Timer = $CooldownTimer
@@ -17,22 +19,32 @@ func _process(delta: float) -> void:
 		position += Vector2(SPEED, 0).rotated(rotation)*delta
 	else:
 		rotation = position.direction_to(player.position).angle()
-		position += Vector2(SPEED*0.75, 0).rotated(rotation)*delta
+		position += Vector2(ENRAGED_SPEED, 0).rotated(rotation)*delta
 	
 	if raycast.is_colliding():
 		rotate_randomly()
 
 func rotate_randomly():
 	# random rotation angle in degrees
-	var random_rotations = [90, 180, 270]
+	var random_rotations = ANGLES.filter(func (item):
+		return item != rotation
+	)
 	rotation += deg_to_rad(random_rotations.pick_random())
 	
 # for the trigger collision
 func _on_body_entered(body: Node2D) -> void:
 	# the thing gets enraged
-	cooldownTimer.start()
 	animationPlayer.play("scale")
 	enraged = true
+	
+	# find the nearest value and use that rotation
+	var diffAngle = ANGLES.map(func (item):
+		return abs(rotation-item)
+	)
+	var index = diffAngle.find(func (item):
+		return item == diffAngle.min()
+	)
+	rotation = ANGLES[index]
 
 # for the kill collision
 # TODO: add the player die thing here
@@ -44,3 +56,7 @@ func _on_cooldown_timer_timeout() -> void:
 	enraged=false
 	# reset the player rotation
 	rotation = 0
+
+func _on_body_exited(body: Node2D) -> void:
+	# when the player moves outside the spider then, cooldown timer starts else it remains enraged
+	cooldownTimer.start()
