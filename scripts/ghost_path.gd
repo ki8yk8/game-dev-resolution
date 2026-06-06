6f78df3de39997a5252409a79e18e2413de441cb
ki8yk8 <ki8yk8@gmail.com>
2026-06-06 14:53:52 +0545

    add full track with better path follow

--- scripts/ghost_path.gd
+++ scripts/ghost_path.gd
@@ -1,13 +1,4 @@
 extends PathFollow2D
 
-var direction = 1;
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress_ratio += 0.1*direction*delta
-	
-	# invert the direction
-	if progress_ratio == 1.0:
-		direction = -1
-	if progress_ratio == 0:
-		direction = 1
+	progress_ratio += 0.4
