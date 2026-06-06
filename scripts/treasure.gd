a1a54345a05700a80b828f87ce6a92d408acd19f
ki8yk8 <ki8yk8@gmail.com>
2026-06-07 00:39:08 +0545

    adds the coins in trasure

--- scripts/treasure.gd
+++ scripts/treasure.gd
@@ -1,8 +1,10 @@
 extends StaticBody2D
 
+const COINS_FROM_TREASURE: int = 10
 @onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var treasureSfx = $traasureSfx
 
 func _on_collision_mat_body_entered(body: Node2D) -> void:
 	animatedSprite.play("open")
 	treasureSfx.play()
+	GameManager.update_coins(COINS_FROM_TREASURE)
