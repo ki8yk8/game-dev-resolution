af0a178e833de59de26e4c1c074bd6956be7991d
ki8yk8 <ki8yk8@gmail.com>
2026-06-07 09:17:31 +0545

    creates reusable survive attack layout with godot language

---
+++ scripts/survive_attack.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+const SHOOTERS_IN_A_ROW:int = 12
+const SHOOTER_SPACING: int = 14
+
+var shooterRows: Array[Node] = []
+@export var attackRow: PackedScene = preload("res://scenes/survive_attack_row.tscn")
+@export var shooter: PackedScene = preload("res://scenes/survive_attach_shooter.tscn")
+@onready var collisionShape: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	var half_size: float = SHOOTERS_IN_A_ROW*SHOOTER_SPACING/2.0
+		
+	# spawn four attack rows and for each spawn the shooter
+	for i in range(4):
+		var s = attackRow.instantiate()
+		shooterRows.append(s)
+		add_child(s)
+		
+		# positioning
+		s.rotation_degrees = 90*i
+		match i:
+			0: s.position = Vector2(0, -half_size-4)
+			1: s.position = Vector2(half_size, 0)
+			2: s.position = Vector2(0, half_size+4)
+			3: s.position = Vector2(-half_size, 0)
+		
+		var actualShooters: Array[Node] = []
+		for j in range(SHOOTERS_IN_A_ROW):
+			var sword = shooter.instantiate()
+			s.add_child(sword)
+			sword.position.x = (j*SHOOTER_SPACING) - half_size + (SHOOTER_SPACING/2.0)
--- scripts/survive_attack_row.gd
+++ scripts/survive_attack_row.gd
@@ -3,14 +3,6 @@ class_name SurviveAttackRow
 
 var shooters: Array[Node] = []
 
-func _ready() -> void:
-	var children = get_children()
-	var pushTrapShooterChildren = children.filter(func (item):
-		return item is PushTrapShooter
-	)
-	assert(len(pushTrapShooterChildren) > 0)
-	shooters = pushTrapShooterChildren
-
 func shoot():
 	for shooter in shooters:
 		shooter.shoot()
