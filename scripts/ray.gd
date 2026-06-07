24c556a97078af57483b7888edecf6777bda25d4
ki8yk8 <ki8yk8@gmail.com>
2026-06-08 00:33:39 +0545

    adds the ray boundary to remove unwanted photons;

--- scripts/ray.gd
+++ scripts/ray.gd
@@ -2,3 +2,7 @@ extends Area2D
 
 func _process(delta: float) -> void:
 	position += Vector2(80*delta, 0).rotated(rotation)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is RayBoundary:
+		queue_free()
