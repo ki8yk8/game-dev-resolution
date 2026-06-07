a43de543e9615235e43fca6f948fd81665722906
ki8yk8 <ki8yk8@gmail.com>
2026-06-07 23:00:11 +0545

    the breakable tile now breaks when hit by the bullet

--- scripts/breakable_wall.gd
+++ scripts/breakable_wall.gd
@@ -1,4 +1,5 @@
 extends StaticBody2D
+class_name BreakableWall
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Bullet:
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -11,3 +11,9 @@ func _process(delta: float) -> void:
 
 func _on_bullet_timer_timeout() -> void:
 	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is BreakableWall:
+		bulletTimer.stop()
+		body.queue_free()
+		queue_free()
