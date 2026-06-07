ef830fef28a4f91e9bf18abb4e13e0d3aba1dd72
ki8yk8 <ki8yk8@gmail.com>
2026-06-07 14:32:42 +0545

    sync player rotation with the rotation of scorpion

--- scripts/scorpion.gd
+++ scripts/scorpion.gd
@@ -2,3 +2,8 @@ extends CharacterBody2D
 class_name Scorpion
 
 var scorpionActive: bool = false
+
+@onready var player: CharacterBody2D = $"../../Player"
+
+func _process(delta: float) -> void:
+	look_at(player.global_position)
